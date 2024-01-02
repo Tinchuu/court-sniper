62c442225f1bd1dab887ce518829250fe4e6a495
Tinchuu <65702538+Tinchuu@users.noreply.github.com>
2024-01-02 14:27:25 +1300

    feat: added embed to allow for checking of all courts within the time period

--- commands/check_session_availability.py
+++ commands/check_session_availability.py
@@ -8,12 +8,15 @@ async def check_session_availability(interaction: discord.Interaction, start: in
     start_date = date.replace(hour=start)
     end_date = date.replace(hour=end)
 
-    print(start_date)
-    print(end_date)
+    embed = discord.Embed(title="Result", url="https://platform.aklbadminton.com/booking", description="List of courts", color=0xff0000)
 
     try:
-        session_info = await search.query_court(start_date, end_date, 1)
-        await interaction.response.send_message(session_info.content)
+        for i in range(1, 13):
+            session_info = await search.query_court(start_date, end_date, i)
+            embed.add_field(name=f"Court {i}", value=json.loads(session_info.content)["meta"]["error"], inline=False)
+            
+        await interaction.response.send_message(embed=embed)
+
     except Exception as e:
         print(f"An error occurred: {e}")
         await interaction.response.send_message("An error occurred while checking session availability.")
