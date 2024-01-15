b08d5234ea81402db06028a82e90c46dda9a10b6
Tinchuu <65702538+Tinchuu@users.noreply.github.com>
2024-01-15 16:33:12 +1300

    feat: idk what i did but there are a lot of changes

--- commands/check_session_availability.py
+++ commands/check_session_availability.py
@@ -9,32 +9,14 @@ async def check_session_availability(interaction: discord.Interaction, start: in
 
     await interaction.response.send_message("Ok.")
 
-    courts = [x for x in range(end - start)] 
-    index = 0
-    consec = 0
-
-    for hour in range(start, end):
-        start_date = date.replace(hour=hour)
-        end_date = date.replace(hour=hour + 1)
-        courts[index] = []
-        for i in range(1, 13):
-            session_info = await search.query_court(start_date, end_date, i)
-            courts[index].append(not json.loads(session_info.content)["meta"]["error"])
-        index += 1
-    
-
-    for slot in courts:
-        if any(slot):
-            consec += 1
-        else:
-            consec = consec - 1 if consec > 0 else 0
-
-    if consec >= session:
-        print("The session is doable")
+    courts = await search.check_time(start, end, date)
 
     try:
-        embed.add_field(name=f"Court {i}", value=json.loads(session_info.content)["meta"]["error"], inline=False)
-        await interaction.followup.send(courts)
+        index = 0
+        for i in range(start, end):
+            embed.add_field(name=f"Time: {i}", value=courts[index], inline=False)
+            index += 1
+        await interaction.followup.send(embed=embed)
 
     except Exception as e:
         print(f"An error occurred: {e}")
--- service/SearchService.py
+++ service/SearchService.py
@@ -1,7 +1,25 @@
 from datetime import datetime
+import json
 from repository.SearchRepo import SearchRepo
+import numpy
 
 class SearchService:
+
+    async def check_time(self, start: int, end: int, date: datetime):
+        courts = []
+
+        for hour in range(start, end):
+            start_date = date.replace(hour=hour)
+            end_date = date.replace(hour=hour + 1)
+
+            current = []
+            for i in range(1, 13):
+                session_info = await self.query_court(start_date, end_date, i)
+                current.append(not json.loads(session_info.content)["meta"]["error"])
+            courts.append(numpy.where(numpy.array(current))[0].tolist())
+        
+        return courts
+
 
     async def query_court(self, start: datetime, end: datetime, court: int):
         search = SearchRepo()
